65e2b8fdbd421fb681424d3d5312d49e8b8b6ead
Alejandro Aguilar Méndez <alejandrooaguilarmendez@gmail.com>
2025-11-24 14:13:12 +0100

    Controlador del robot

---
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/ControladorRobot.java
@@ -0,0 +1,24 @@
+package org.iesalandalus.programacion.robot.modelo;
+
+import java.util.Objects;
+
+public class ControladorRobot {
+    private Robot robot;
+
+    public ControladorRobot(Robot robot) {
+        Objects.requireNonNull(robot, "El robot no puede ser nulo.");
+    }
+
+    public Robot getRobot() {
+        return new Robot();
+    }
+
+    public void ejecutar(char comando) throws RobotExcepcion{
+        switch (comando){
+            case 'A', 'a' -> robot.avanzar();
+            case 'D', 'd' -> robot.girarALaDerecha();
+            case 'I', 'i' -> robot.girarALaIzquierda();
+            default -> throw new RobotExcepcion("Comando desconocido.");
+        }
+    }
+}
